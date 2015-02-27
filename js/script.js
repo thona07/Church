b33d9d712982e2f40bd86e912234f3faffcf7ea3
thona07 <eukthona06@gmail.com>
2015-02-27 16:57:58 +0700

    Update code style in folder church

--- js/script.js
+++ js/script.js
@@ -3,3 +3,21 @@
  *	Developer		:		Glean Team
  *	Date			:		26-February-2015
  */
+ 
+$( function() {
+	var Church = {
+		init: function() {
+			this.toggleMenu();
+		},
+		
+		toggleMenu: function() {
+			$('nav .menu').click( function() {
+				$('nav ul').slideToggle( 200 );
+			});
+		}
+	}
+	
+	$( document ).ready( function() {
+		Church.init();
+	});
+});
